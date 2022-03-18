de28add718eaaa63a2db156bbeba4c308c9849f9
guchio33 <aokihikari3@gmail.com>
2022-03-19 03:57:12 +0900

    questionモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :questions
   root 'home#index'
   get 'home/index'
-  devise_for :users, controllers: {
-    session: 'users/sessions'
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations', #app/controllers/users/registrations参照 ユーザ登録
+    session: 'users/sessions'#app/controllers/users/sessions参照 ログイン
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
